ddbfef1dc762590bacd069ae7c37c14fee5e060e
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-17 00:29:06 +0000

    [Finish]応用課題2デバッグ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -20,27 +22,35 @@ class BooksController < ApplicationController
   end
 
   def edit
+     book = Book.find(params[:id])
+  unless book.user_id == current_user.id
+    redirect_to books_path
+  end
     @book = Book.find(params[:id])
   end
 
   def update
+     book = Book.find(params[:id])
+  unless book.user_id == current_user.id
+    redirect_to books_path
+  end
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render 'edit'
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user
 
   def show
     @user = User.find(params[:id])
@@ -10,15 +11,25 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to user_path(current_user.id)
+  end
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to user_path(current_user.id)
+  end
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -32,6 +43,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
+    end
+  end
+
+  def authenticate_user
+    unless user_signed_in?
+    redirect_to new_user_session_path
     end
   end
 end
