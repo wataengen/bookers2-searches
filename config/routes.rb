237cf2589d6eda075598162d53d652baac68a9e1
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-18 02:42:10 +0000

    [Update]いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about"=>"home#about"
 
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+   resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
 
 end
